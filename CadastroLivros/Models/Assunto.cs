d8869dffaf99c03ba4a0cbf16ba1bc80e5b26b59
Izoldino Amaral <dino.amaral@outlook.com>
2024-08-13 11:11:13 -0300

    Ajustes Assunto

--- CadastroLivros/Models/Assunto.cs
+++ CadastroLivros/Models/Assunto.cs
@@ -8,7 +8,7 @@ namespace CadastroLivros.Models
         [Required(ErrorMessage = "Digite o Assunto")]
         [StringLength(20)]
         public string Descricao { get; set; }
-        public virtual ICollection<Livro> Livros { get; set; }
+        public virtual ICollection<Livro>? Livros { get; set; }
 
         public ICollection<LivroAssunto>? LivroAssuntos { get; set; }
     }
